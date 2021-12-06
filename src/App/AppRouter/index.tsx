fd5d8a5326beb684a032b52319729acbef224aa6
Hiroshi <hiroshi.yoshida1228@gmail.com>
2021-12-06 17:35:59 -0300

    add blank explore and blindbox pages

--- src/App/AppRouter/index.tsx
+++ src/App/AppRouter/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from 'src/components/Layout';
+import BlindBoxPage from 'src/components/pages/BlindBoxPage';
+import ExplorePage from 'src/components/pages/ExplorePage';
 import HomePage from 'src/components/pages/HomePage';
 import ProfilePage from 'src/components/pages/ProfilePage';
 
 const AppRouter: React.FC = (): JSX.Element => {
     return (
-        <Layout>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<HomePage />} />
-                    <Route path="/profile" element={<ProfilePage />} />
-                </Routes>
-            </BrowserRouter>
-        </Layout>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Layout><HomePage /></Layout>} />
+                <Route path="/explore" element={<Layout><ExplorePage /></Layout>} />
+                <Route path="/blind-box" element={<Layout><BlindBoxPage /></Layout>} />
+                <Route path="/profile" element={<Layout><ProfilePage /></Layout>} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 
---
+++ src/components/pages/BlindBoxPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BlindBoxPage: React.FC = (): JSX.Element => {
+    return <>This is blind box page</>
+};
+
+export default BlindBoxPage;
