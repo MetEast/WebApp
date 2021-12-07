13876e4bb8b42457ea88ac33875aaa89472bee65
Hiroshi <hiroshi.yoshida1228@gmail.com>
2021-12-07 10:22:18 -0300

    override mui button styles

--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -16,6 +16,18 @@ theme.components = {
         styleOverrides: {
             root: {
                 textTransform: 'none',
+                borderRadius: '12px',
+                fontWeight: 500,
+                fontSize: '1rem',
+                lineHeight: '1.125rem',
+                paddingTop: '0.75rem',
+                paddingBottom: '0.75rem',
+            },
+            contained: {
+                backgroundColor: 'var(--color-base)',
+            },
+            outlined: {
+                color: 'var(--color-base)',
             },
         },
     },
