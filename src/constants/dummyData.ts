93a773bae052563118557be645d37c5e7c087535
Hiroshi <hiroshi.yoshida1228@gmail.com>
2021-12-08 18:05:53 -0300

    add dummy data for products and notifications

---
+++ src/constants/dummyData.ts
@@ -0,0 +1,73 @@
+import { TypeNotification } from "src/types/notification-types";
+import { TypeProduct } from "src/types/product-types";
+
+export const dummyNotificationList: Array<TypeNotification> = [
+    {
+        title: 'You have a new bid!',
+        content:
+            'Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00',
+        date: '2021-09-03',
+    },
+    {
+        title: 'You have a new bid!',
+        content: 'Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00',
+        date: '2021-09-03',
+    },
+    {
+        title: 'You have a new bid!',
+        content: 'Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00',
+        date: '2021-09-03',
+        isRead: true,
+    },
+    {
+        title: 'You have a new bid!',
+        content: 'Your CryptoGirl#19 project has just been bid by a VKWR909981 user for E 100.00',
+        date: '2021-09-03',
+        isRead: true,
+    },
+];
+
+export const dummyProducts: Array<TypeProduct> = [
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+    {
+        image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2021/03/Advanced-NFT-Deployment---1-.png',
+        name: 'Example product',
+        price: 100,
+        likes: 10,
+    },
+];
