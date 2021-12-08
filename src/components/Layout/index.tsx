88af56a7173d7e87b6dc95071b66685f05be165e
Hiroshi <hiroshi.yoshida1228@gmail.com>
2021-12-08 13:14:34 -0300

    fix padding of layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 const Layout: React.FC = ({ children }): JSX.Element => {
     return (
         <>
-            <Box pt={4} pb={16} display="flex" justifyContent="center">
+            <Box p={4} pb={16} display="flex" justifyContent="center">
                 <Box width="100%" maxWidth="800px">
                     {children}
                 </Box>
