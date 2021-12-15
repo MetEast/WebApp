09bfbec672e894ad0d97c4add70085247bde4335
Hiroshi <hiroshi.yoshida1228@gmail.com>
2021-12-15 13:58:20 -0300

    add back button to edit profile page

--- src/components/profile/EditProfile/index.tsx
+++ src/components/profile/EditProfile/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Box, Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export interface IEditProfileProps {}
 
 const EditProfile: React.FC<IEditProfileProps> = (): JSX.Element => {
-    return <>edit profile</>
+    const navigate = useNavigate();
+
+    const handleClickBack = () => {
+        navigate('/profile');
+    };
+    
+    return (
+        <Box>
+            <Box>
+                <Button variant="outlined" onClick={handleClickBack}>
+                    Back
+                </Button>
+            </Box>
+        </Box>
+    );
 };
 
 export default EditProfile;
