e4e4ca44f51668f904cb9bec2583c465278c6bec
TrueLifeDev <truelife1228@outlook.com>
2022-01-05 11:55:43 -0600

    MyNFTCreated page updated

--- src/pages/MyNFT/Created/index.tsx
+++ src/pages/MyNFT/Created/index.tsx
@@ -7,15 +7,46 @@ import ProductSnippets from 'src/components/ProductSnippets';
 import ProductBadge from 'src/components/ProductBadge';
 import ELAPrice from 'src/components/ELAPrice';
 import { PrimaryButton } from 'src/components/Buttons/styles';
-import SingleNFTMoreInfo from 'src/components/SingleNFTMoreInfo';
-import SingleNFTBidsTable from 'src/components/SingleNFTBidsTable';
-import PriceHistoryView from 'src/components/PriceHistoryView';
+import AboutAuthor from 'src/components/SingleNFTMoreInfo/AboutAuthor';
+import ProjectDescription from 'src/components/SingleNFTMoreInfo/ProjectDescription';
+import ChainDetails from 'src/components/SingleNFTMoreInfo/ChainDetails';
+import ProductTransHistory from 'src/components/ProductTransHistory';
 
 const MyNFTCreated: React.FC = (): JSX.Element => {
     return (
         <>
             <ProductPageHeader />
-            MyNFTCreated
+            <Grid container marginTop={5} columnSpacing={5}>
+                <Grid item xs={6}>
+                    <ProductImageContainer imgurl={'/assets/images/mynft/mynft-template2.png'} />
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography fontSize={56} fontWeight={700}>
+                        Sculpting with the Heart
+                    </Typography>
+                    <ProductSnippets sold={24} instock={200} likes={88} views={4800} />
+                    <Stack direction="row" alignItems="center" spacing={1} marginTop={3}>
+                        <ProductBadge badgeType={enumBadgeType.Created} />
+                        <ProductBadge badgeType={enumBadgeType.Museum} />
+                    </Stack>
+                    {/* <ELAPrice ela_price={199} usd_price={480} marginTop={3} /> */}
+                    <PrimaryButton sx={{ marginTop: 3, width: '100%' }}>Sell</PrimaryButton>
+                </Grid>
+            </Grid>
+            <Grid container marginTop={5} columnSpacing={5}>
+                <Grid item xs={6}>
+                    <Stack spacing={3}>
+                        <AboutAuthor />
+                        <ProductTransHistory sold={false} bought={false} />
+                    </Stack>
+                </Grid>
+                <Grid item xs={6}>
+                    <Stack spacing={3}>
+                        <ProjectDescription />
+                        <ChainDetails />
+                    </Stack>
+                </Grid>
+            </Grid>
         </>
     );
 };
