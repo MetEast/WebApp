0c122c25de8dcf31412c3010b2af3da62c55d629
TrueLifeDev <truelife1228@outlook.com>
2022-01-05 08:04:14 -0600

    PriceHistoryView updated

--- src/components/PriceHistoryView/index.tsx
+++ src/components/PriceHistoryView/index.tsx
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Chart from 'react-apexcharts';
+import PriceHistoryToolTip from './tooltip';
+import { renderToString } from 'react-dom/server';
 
 interface ComponentProps {}
 
 const PriceHistoryView: React.FC<ComponentProps> = (): JSX.Element => {
+    const tooltipBox = ({
+        series,
+        seriesIndex,
+        dataPointIndex,
+        w,
+    }: {
+        series: any;
+        seriesIndex: any;
+        dataPointIndex: any;
+        w: any;
+    }) => {
+        return renderToString(
+            <PriceHistoryToolTip
+                price={series[seriesIndex][dataPointIndex]}
+                timestamp={w.globals.seriesX[seriesIndex][dataPointIndex]}
+            />,
+        );
+    };
+
+    const options = {
+        chart: {
+            id: 'area-bar',
+        },
+        dataLabels: { enabled: false },
+        grid: {
+            show: true,
+            xaxis: {
+                lines: {
+                    show: true,
+                },
+            },
+            yaxis: {
+                lines: {
+                    show: true,
+                },
+            },
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shadeIntensity: 1,
+                opacityFrom: 0.5,
+                opacityTo: 0.9,
+            },
+        },
+        xaxis: {
+            type: 'datetime' as const,
+            labels: {
+                format: 'MMM',
+            },
+        },
+        tooltip: {
+            custom: tooltipBox,
+        },
+    };
+    const series = [
+        {
+            data: [
+                { x: '01/01/2021', y: 10 },
+                { x: '01/21/2021', y: 60 },
+                { x: '02/06/2021', y: 40 },
+                { x: '03/06/2021', y: 30 },
+                { x: '04/06/2021', y: 20 },
+                { x: '04/17/2021', y: 30 },
+                { x: '05/06/2021', y: 40 },
+                { x: '06/06/2021', y: 60 },
+                { x: '07/06/2021', y: 50 },
+                { x: '08/06/2021', y: 40 },
+                { x: '09/06/2021', y: 60 },
+                { x: '10/06/2021', y: 80 },
+                { x: '11/06/2021', y: 90 },
+                { x: '12/06/2021', y: 70 },
+            ],
+        },
+    ];
+
+    const [chartOptions, setChartOptions] = useState(options);
+    const [chartSeries, setChartSeries] = useState(series);
+
     return (
         <Box>
             <Stack direction="row" alignItems="center" justifyContent="space-between">
@@ -11,6 +93,7 @@ const PriceHistoryView: React.FC<ComponentProps> = (): JSX.Element => {
                     Price History
                 </Typography>
             </Stack>
+            <Chart options={chartOptions} series={chartSeries} type="area" />
         </Box>
     );
 };
---
+++ src/components/PriceHistoryView/tooltip.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import ELAPrice from 'src/components/ELAPrice';
+
+interface ComponentProps {
+    price: number;
+    timestamp: number;
+}
+
+const PriceHistoryToolTip: React.FC<ComponentProps> = ({ price, timestamp }): JSX.Element => {
+    const getDateString = (timestamp: number) => {
+        const t = new Date(timestamp);
+        const date = ('0' + t.getDate()).slice(-2);
+        const month = ('0' + (t.getMonth() + 1)).slice(-2);
+        const year = t.getFullYear();
+        const hours = ('0' + t.getHours()).slice(-2);
+        const minutes = ('0' + t.getMinutes()).slice(-2);
+        const seconds = ('0' + t.getSeconds()).slice(-2);
+        const time = `${year}-${month}-${date}`;
+        return time;
+    };
+
+    return (
+        <Stack padding={2} sx={{ background: 'white' }}>
+            <ELAPrice ela_price={price} />
+            <Typography fontSize={12} fontWeight={400}>
+                {getDateString(timestamp)}
+            </Typography>
+            <Typography fontSize={12} fontWeight={700} alignSelf="flex-end">
+                Username
+            </Typography>
+        </Stack>
+    );
+};
+
+export default PriceHistoryToolTip;
