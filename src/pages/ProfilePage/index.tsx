07a0f09b1c2b2fecf7269fccfc6446b8ff422a98
TrueLifeDev <truelife1228@outlook.com>
2022-01-12 08:43:57 -0600

    Removed old profil page components

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import Notifications from 'src/components/Notifications';
-import PrivateProfile from 'src/components/profile/PrivateProfile';
 import PrivateProfilePage from './profile';
 import ModalDialog from 'src/components/ModalDialog';
 import ConnectDID from 'src/components/profile/ConnectDID';
-import ConnectWallet from 'src/components/profile/ConnectWallet';
 import ChooseWallet from 'src/components/profile/ChooseWallet';
 
 const ProfilePage: React.FC = (): JSX.Element => {
@@ -19,9 +17,6 @@ const ProfilePage: React.FC = (): JSX.Element => {
             <PrivateProfilePage />
             {/* <ModalDialog open={true} onClose={() => {}}>
                 <ConnectDID onClickConnect={() => {}}/>
-            </ModalDialog> */}
-            {/* <ModalDialog open={true} onClose={() => {}}>
-                <ConnectWallet onClickConnect={() => {}} onClickNotifications={() => {}}/>
             </ModalDialog> */}
             {/* <ModalDialog open={true} onClose={() => {}}>
                 <ChooseWallet onClickClose={() => {}}/>
