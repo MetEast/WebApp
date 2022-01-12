30ddea49f1d8fbc434626f89f6eac8909ce43d83
TrueLifeDev <truelife1228@outlook.com>
2022-01-12 17:11:47 -0600

    DialogContext updated

--- src/context/DialogContext.tsx
+++ src/context/DialogContext.tsx
@@ -1,12 +1,6 @@
-import React, { useReducer, createContext, useMemo, useContext } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
-export enum ActionType {
-    SET_STATE = 'SET_STATE',
-}
-
-export type Actions = { type: ActionType.SET_STATE; value: State };
-
-export interface State {
+interface State {
     createNFTDlgOpened: boolean;
 }
 
@@ -14,31 +8,19 @@ const defaultState: State = {
     createNFTDlgOpened: false,
 };
 
-function noop() {
-    return;
-}
-
 type ContextType<TValue> = [TValue, (newValue: TValue) => void];
 
-const defaultContextValue: ContextType<State> = [defaultState, noop];
-
-const reducer = (state: State, action: Actions): State => {
-    switch (action.type) {
-        case ActionType.SET_STATE:
-            return action.value;
-        default:
-            return state;
-    }
-};
+const defaultContextValue: ContextType<State> = [defaultState, () => {}];
 
 export const DialogContext = createContext(defaultContextValue);
 
 export const DialogContextProvider: React.FC = ({ children, ...props }) => {
-    const [state, dispatch] = useReducer(reducer, defaultState);
+    const [contextState, setContextState] = useState<State>(defaultState);
+
     const ctxValue: ContextType<State> = [
-        state,
+        contextState,
         (value: State) => {
-            dispatch({ type: ActionType.SET_STATE, value });
+            setContextState(value);
         },
     ];
 
