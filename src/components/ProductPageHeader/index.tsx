7714653fff4b6a555869a592bd019ceea6782b0b
TrueLifeDev <truelife1228@outlook.com>
2022-01-17 09:21:42 -0600

    Enabled Back button to back to previous page in Details page

--- src/components/ProductPageHeader/index.tsx
+++ src/components/ProductPageHeader/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Stack } from '@mui/material';
 import { Icon } from '@iconify/react';
 import { PrimaryBtn, IconBtn } from './styles';
+import { useNavigate } from 'react-router-dom';
 
 const ProductPageHeader: React.FC = (): JSX.Element => {
+    const navigate = useNavigate();
+
     return (
         <Stack direction="row" justifyContent="space-between">
-            <PrimaryBtn startIcon={<Icon icon="ph:caret-left-bold" color="#1890FF" />}>Back</PrimaryBtn>
+            <PrimaryBtn
+                startIcon={<Icon icon="ph:caret-left-bold" color="#1890FF" />}
+                onClick={() => {
+                    navigate(-1);
+                }}
+            >
+                Back
+            </PrimaryBtn>
             <Stack direction="row" spacing={1}>
                 <PrimaryBtn startIcon={<Icon icon="ph:share-network-bold" color="#1890FF" />}>Share</PrimaryBtn>
                 <IconBtn>
