75999fecf6da7a45bb262d966c6b8563eb7d3160
TrueLifeDev <truelife1228@outlook.com>
2022-01-17 09:32:21 -0600

    Updated to support to cancel displaying of WalletConnect/Signin with DID

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -4,29 +4,29 @@ import ConnectDID from 'src/components/profile/ConnectDID';
 import ChooseWallet from 'src/components/profile/ChooseWallet';
 import PrivateProfilePage from './profile';
 
-import { useWeb3React } from "@web3-react/core";
-import { Web3Provider } from "@ethersproject/providers";
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 import {
     NoEthereumProviderError,
     UserRejectedRequestError as UserRejectedRequestErrorInjected,
-  } from "@web3-react/injected-connector";
-import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from "@web3-react/walletconnect-connector";
-import { UnsupportedChainIdError } from "@web3-react/core";
-import { injected, walletconnect, walletlink } from "src/components/ConnectWalletButton/connectors";
-import { useEagerConnect, useInactiveListener } from "src/components/ConnectWalletButton/hook";
+} from '@web3-react/injected-connector';
+import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector';
+import { UnsupportedChainIdError } from '@web3-react/core';
+import { injected, walletconnect, walletlink } from 'src/components/ConnectWalletButton/connectors';
+import { useEagerConnect, useInactiveListener } from 'src/components/ConnectWalletButton/hook';
 
 function getErrorMessage(error: Error) {
     if (error instanceof NoEthereumProviderError) {
-        return "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.";
+        return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.';
     } else if (error instanceof UnsupportedChainIdError) {
         return "You're connected to an unsupported network.";
     } else if (
         error instanceof UserRejectedRequestErrorInjected ||
         error instanceof UserRejectedRequestErrorWalletConnect
     ) {
-        return "Please authorize this website to access your Ethereum account.";
+        return 'Please authorize this website to access your Ethereum account.';
     } else {
-        return "An unknown error occurred. Check the console for more details.";
+        return 'An unknown error occurred. Check the console for more details.';
     }
 }
 
@@ -47,31 +47,37 @@ const ProfilePage: React.FC = (): JSX.Element => {
         console.log(active, library, chainId);
         setActivatingConnector(undefined);
         if (active) {
-          setShowModal(false);
-          setStep(2);
+            setShowModal(false);
+            setStep(2);
         }
         if (error) {
-          setErrors([getErrorMessage(error)]);
+            setErrors([getErrorMessage(error)]);
         }
-      }, [active, error, library, chainId]);
-    
-      const triedEager = useEagerConnect();
-    
-      useInactiveListener(!triedEager || !!activatingConnector);
-    
-      const handleClickConnect = async (wallet: 'walletconnect' | 'elastos' | 'metamask') => {
+    }, [active, error, library, chainId]);
+
+    const triedEager = useEagerConnect();
+
+    useInactiveListener(!triedEager || !!activatingConnector);
+
+    const handleClickConnect = async (wallet: 'walletconnect' | 'elastos' | 'metamask') => {
         let currentConnector: any = null;
-        if(wallet === 'metamask') currentConnector = injected;
-        else if(wallet === 'elastos') currentConnector = walletlink;
-        else if(wallet === 'walletconnect') currentConnector = walletconnect;
+        if (wallet === 'metamask') currentConnector = injected;
+        else if (wallet === 'elastos') currentConnector = walletlink;
+        else if (wallet === 'walletconnect') currentConnector = walletconnect;
         setActivatingConnector(currentConnector);
         await activate(currentConnector);
-      };
+    };
 
     return (
         <>
             {(step === 0 || step === 1) && (
-                <ModalDialog open={showModal} onClose={() => {}}>
+                <ModalDialog
+                    open={showModal}
+                    onClose={() => {
+                        setShowModal(false);
+                        setStep(2);
+                    }}
+                >
                     {step === 0 && <ConnectDID onConnect={handleClick(1)} />}
                     {step === 1 && <ChooseWallet onConnect={handleClickConnect} />}
                 </ModalDialog>
