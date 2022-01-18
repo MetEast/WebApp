729379a4d61f60657b67f8f480ff302e39d6f2b5
TrueLifeDev <truelife1228@outlook.com>
2022-01-18 11:39:40 -0600

    Admin BlindBoxes page table data added

--- src/pages/Admin/BlindBoxes/index.tsx
+++ src/pages/Admin/BlindBoxes/index.tsx
@@ -1,8 +1,55 @@
-import React, { useState } from 'react';
-import { Box, Stack } from '@mui/material';
+import React, { useState, useMemo } from 'react';
+import Table from 'src/components/Admin/Table';
+import { AdminBlindBoxItemType, AdminTableHeadCell } from 'src/types/admin-table-data-types';
 
 const AdminBlindBoxes: React.FC = (): JSX.Element => {
-    return <>AdminBlindBoxes</>;
+    const headCells: AdminTableHeadCell[] = [
+        {
+            id: 'blindbox_id',
+            label: 'Blind Box ID',
+        },
+        {
+            id: 'blindbox_name',
+            label: 'Blind Box name',
+        },
+        {
+            id: 'status',
+            label: 'Status',
+        },
+        {
+            id: 'price',
+            label: 'Price',
+        },
+        {
+            id: 'sale_begins',
+            label: 'Sale begins',
+        },
+        {
+            id: 'sale_ends',
+            label: 'Sale Ends',
+        },
+    ];
+
+    const data: AdminBlindBoxItemType[] = useMemo(
+        () =>
+            [...Array(136).keys()].map(
+                (item) =>
+                    ({
+                        id: item,
+                        blindbox_id: String(item + 1).padStart(4, '0'),
+                        blindbox_name: 'Blind Box Title',
+                        status: 'online',
+                        price: 199,
+                        sale_begins: '2022-06-18  08:50:00',
+                        sale_ends: '2022-06-18  08:50:00',
+                    } as AdminBlindBoxItemType),
+            ),
+        [],
+    );
+
+    const [tabledata, setTabledata] = useState(data);
+
+    return <Table tabledata={tabledata} headCells={headCells} />;
 };
 
 export default AdminBlindBoxes;
