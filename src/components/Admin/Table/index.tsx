f6a60f92586ac2bea9300852f1d997220f892ede
TrueLifeDev <truelife1228@outlook.com>
2022-01-18 21:19:28 -0600

    Admin table compre functions splitted

--- src/components/Admin/Table/index.tsx
+++ src/components/Admin/Table/index.tsx
@@ -17,10 +17,10 @@ import IconButton from '@mui/material/IconButton';
 import { DataTable, PageButton } from './styles';
 import Select from 'src/components/Admin/Select';
 import { TypeSelectItem } from 'src/types/select-types';
+import { Order } from 'src/types/order';
 import { Icon } from '@iconify/react';
 import { AdminTableItemType, AdminTableHeadCell } from 'src/types/admin-table-data-types';
-
-type Order = 'asc' | 'desc';
+import { stableSort, getComparator } from './comparefunc';
 
 interface EnhancedTableProps {
     numSelected: number;
@@ -77,39 +77,6 @@ function EnhancedTableHead(props: EnhancedTableProps) {
             </TableRow>
         </TableHead>
     );
-}
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator<Key extends keyof any>(
-    order: Order,
-    orderBy: Key,
-): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// This method is created for cross-browser compatibility, if you don't
-// need to support IE11, you can use Array.prototype.sort() directly
-function stableSort<T>(array: readonly T[], comparator: (a: T, b: T) => number) {
-    const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) {
-            return order;
-        }
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
 }
 
 interface ComponentProps {
