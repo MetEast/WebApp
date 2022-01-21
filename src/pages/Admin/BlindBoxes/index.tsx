88f9a0f5eefbbd90f0d51f0f442ab5dcab3cbb1e
TrueLifeDev <truelife1228@outlook.com>
2022-01-21 14:58:18 -0600

    Admin BlindBoxes option box added

--- src/pages/Admin/BlindBoxes/index.tsx
+++ src/pages/Admin/BlindBoxes/index.tsx
@@ -1,6 +1,10 @@
 import React, { useState, useMemo } from 'react';
+import { Typography, Stack, IconButton } from '@mui/material';
 import Table from 'src/components/Admin/Table';
 import { AdminBlindBoxItemType, AdminTableColumn } from 'src/types/admin-table-data-types';
+import CustomTextField from 'src/components/TextField';
+import { PrimaryButton } from 'src/components/Buttons/styles';
+import { Icon } from '@iconify/react';
 
 const AdminBlindBoxes: React.FC = (): JSX.Element => {
     const columns: AdminTableColumn[] = [
@@ -49,7 +53,24 @@ const AdminBlindBoxes: React.FC = (): JSX.Element => {
 
     const [tabledata, setTabledata] = useState(data);
 
-    return <Table tabledata={tabledata} columns={columns} />;
+    return (
+        <Stack height="100%" spacing={4}>
+            <Stack direction="row" alignItems="flex-end" spacing={1}>
+                <CustomTextField title="Blind Box ID" placeholder="Enter ID" />
+                <CustomTextField title="Blind Box Name" placeholder="Enter Name" />
+                <PrimaryButton size="small" sx={{ paddingX: 3 }}>
+                    <Icon
+                        icon="ph:magnifying-glass"
+                        fontSize={20}
+                        color="white"
+                        style={{ marginBottom: 2, marginRight: 4 }}
+                    />
+                    {`Search`}
+                </PrimaryButton>
+            </Stack>
+            <Table tabledata={tabledata} columns={columns} />
+        </Stack>
+    );
 };
 
 export default AdminBlindBoxes;
