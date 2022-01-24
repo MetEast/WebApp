cdb0dd2527e9b85ad0f74f05e2d83a3f4236486b
TrueLifeDev <truelife1228@outlook.com>
2022-01-24 02:40:07 -0600

    Admin HomePopular page AddPopularItem dlg updated

--- src/components/Admin/Dialogs/AddPopularItem/index.tsx
+++ src/components/Admin/Dialogs/AddPopularItem/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Stack, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import { DialogTitleTypo } from '../styles';
 import { PrimaryButton, SecondaryButton } from 'src/components/Buttons/styles';
+import SearchTextField from '../components/SearchTextField';
+import CustomTextField from 'src/components/TextField';
 
 export interface ComponentProps {}
 
@@ -10,6 +12,15 @@ const AddPopularItem: React.FC<ComponentProps> = (): JSX.Element => {
         <Stack spacing={5} width={320}>
             <Stack alignItems="center">
                 <DialogTitleTypo>Add Popular Item</DialogTitleTypo>
+            </Stack>
+            <Stack spacing={3}>
+                <SearchTextField title="Normal NFTs" placeholder="Search NFT" />
+                <SearchTextField title="Blind Boxes" placeholder="Search Blind Box" />
+                <CustomTextField title="Sort" />
+            </Stack>
+            <Stack direction="row" spacing={2}>
+                <SecondaryButton fullWidth>Back</SecondaryButton>
+                <PrimaryButton fullWidth>Confirm</PrimaryButton>
             </Stack>
         </Stack>
     );
