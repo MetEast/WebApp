c644cf83708cde43feca160424a0aa3a1d6f6f45
TrueLifeDev <truelife1228@outlook.com>
2022-01-25 01:21:36 -0600

    AllTransactions Sortby select component added

--- src/components/profile/AllTransactions/index.tsx
+++ src/components/profile/AllTransactions/index.tsx
@@ -3,14 +3,43 @@ import { Stack, Box, Typography } from '@mui/material';
 import { DialogTitleTypo } from 'src/components/ModalDialog/styles';
 import { SecondaryButton } from 'src/components/Buttons/styles';
 import ELAPrice from 'src/components/ELAPrice';
+import Select from '../Select';
+import { TypeSelectItem } from 'src/types/select-types';
 
 export interface ComponentProps {}
 
 const AllTransactions: React.FC<ComponentProps> = (): JSX.Element => {
+    const sortbyOptions: Array<TypeSelectItem> = [
+        {
+            label: 'Option2',
+            value: 'Option2',
+        },
+        {
+            label: 'Option2',
+            value: 'Option2',
+        },
+        {
+            label: 'Option2',
+            value: 'Option2',
+        },
+    ];
+    const [sortby, setSortby] = React.useState<TypeSelectItem>();
+    const handleSortbyChange = (value: string) => {
+        const item = sortbyOptions.find((option) => option.value === value);
+        setSortby(item);
+    };
+
     return (
         <Stack spacing={5} width={520}>
-            <Stack>
+            <Stack direction="row" justifyContent="space-between">
                 <DialogTitleTypo>All Transactions</DialogTitleTypo>
+                <Select
+                    options={sortbyOptions}
+                    selected={sortby}
+                    placeholder="Sort by"
+                    handleClick={handleSortbyChange}
+                    width={160}
+                />
             </Stack>
             <Stack spacing={3}></Stack>
             <SecondaryButton fullWidth>Close</SecondaryButton>
