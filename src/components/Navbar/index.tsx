e755faf84599f15ac42b2b8d8cb153725b959588
blockchaindev91 <tinybear91@outlook.com>
2022-02-25 10:42:26 +0900

    feat: press logo to redirect to homepage

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Stack, IconButton } from '@mui/material';
+import { Box, Typography, Stack, IconButton, Link } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 import { TypeMenuItem } from 'src/types/layout-types';
 import PageButton from './PageButton';
@@ -43,20 +43,19 @@ const Navbar: React.FC<ComponentProps> = ({ mobile = false }): JSX.Element => {
 
     const SignOutWithEssentials = async () => {
         console.log('Signing out user. Deleting session info, auth token');
-        document.cookie += `METEAST_TOKEN=; Path=/; Expires=${new Date().toUTCString()};`
-        document.cookie += `METEAST_DID=; Path=/; Expires=${new Date().toUTCString()};`
-        setSignInDlgState({...signInDlgState, isLoggedIn: false});
+        document.cookie += `METEAST_TOKEN=; Path=/; Expires=${new Date().toUTCString()};`;
+        document.cookie += `METEAST_DID=; Path=/; Expires=${new Date().toUTCString()};`;
+        setSignInDlgState({ ...signInDlgState, isLoggedIn: false });
         try {
             if (isUsingEssentialsConnector() && essentialsConnector.hasWalletConnectSession()) {
                 await essentialsConnector.getWalletConnectProvider().disconnect();
                 // await essentialsConnector.disconnectWalletConnect();
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e);
-        } 
-        if(location.pathname.indexOf('/profile') !== -1 || location.pathname.indexOf('/mynft') !== -1) {
-            navigate('/');   
+        }
+        if (location.pathname.indexOf('/profile') !== -1 || location.pathname.indexOf('/mynft') !== -1) {
+            navigate('/');
         }
         window.location.reload();
     };
@@ -93,8 +92,7 @@ const Navbar: React.FC<ComponentProps> = ({ mobile = false }): JSX.Element => {
                 onClick={() => {
                     if (signInDlgState.isLoggedIn)
                         setDialogState({ ...dialogState, createNFTDlgOpened: true, createNFTDlgStep: 0 });
-                    else setSignInDlgState({...signInDlgState, signInDlgOpened: true});
-
+                    else setSignInDlgState({ ...signInDlgState, signInDlgOpened: true });
                 }}
                 sx={{ paddingX: mobile ? 0 : 2, minWidth: 40 }}
             >
@@ -126,7 +124,7 @@ const Navbar: React.FC<ComponentProps> = ({ mobile = false }): JSX.Element => {
             size="small"
             sx={{ paddingX: 2 }}
             onClick={() => {
-                setSignInDlgState({...signInDlgState, signInDlgOpened: true});
+                setSignInDlgState({ ...signInDlgState, signInDlgOpened: true });
             }}
         >
             <Icon icon="ph:sign-in" fontSize={20} color="white" style={{ marginRight: 4, marginBottom: 2 }} />
@@ -143,12 +141,14 @@ const Navbar: React.FC<ComponentProps> = ({ mobile = false }): JSX.Element => {
             paddingBottom={mobile ? 1.5 : 0}
         >
             {!mobile && (
-                <Stack direction="row" alignItems="center" spacing={1.5}>
-                    <img src="/assets/images/header/logo.svg" alt="" />
-                    <Box display={{ xs: 'none', lg: 'block' }}>
-                        <img src="/assets/images/header/meteast_label.svg" alt="" />
-                    </Box>
-                </Stack>
+                <Link href='/'>
+                    <Stack direction="row" alignItems="center" spacing={1.5}>
+                        <img src="/assets/images/header/logo.svg" alt="" />
+                        <Box display={{ xs: 'none', lg: 'block' }}>
+                            <img src="/assets/images/header/meteast_label.svg" alt="" />
+                        </Box>
+                    </Stack>
+                </Link>
             )}
             {mobile ? (
                 pageButtons
