028b71129cab4b6efa5f2b0ca245f75611f527a5
blockchaindev91 <tinybear91@outlook.com>
2022-03-03 18:59:37 +0900

    feat: add share nfts feature

--- src/components/ProductPageHeader/index.tsx
+++ src/components/ProductPageHeader/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { Stack, Box } from '@mui/material';
+import { Stack, Box, Menu, MenuItem, Link } from '@mui/material';
 import { Icon } from '@iconify/react';
 import { SecondaryButton, PinkButton } from 'src/components/Buttons/styles';
 import { IconBtn } from './styles';
 import { useNavigate } from 'react-router-dom';
+import { FacebookIcon, TwitterIcon } from 'react-share';
+// import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from 'react-share';
+// import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+// import FullscreenIcon from '@mui/icons-material/Fullscreen';
 
 const ProductPageHeader: React.FC = (): JSX.Element => {
     const navigate = useNavigate();
     const [showReportBtn, setShowReportBtn] = React.useState<boolean>(false);
+
+    const [isOpenSharePopup, setOpenSharePopup] = React.useState(null);
+    const openSharePopupMenu = (event: any) => {
+        setOpenSharePopup(event.currentTarget);
+    };
+    const handleCloseSharePopup = () => {
+        setOpenSharePopup(null);
+    };
 
     return (
         <Stack direction="row" justifyContent="space-between">
@@ -27,7 +39,7 @@ const ProductPageHeader: React.FC = (): JSX.Element => {
                 Back
             </SecondaryButton>
             <Stack direction="row" spacing={1}>
-                <SecondaryButton size="small" sx={{ paddingX: 2.5 }}>
+                <SecondaryButton size="small" sx={{ paddingX: 2.5 }} onClick={openSharePopupMenu}>
                     <Icon
                         icon="ph:share-network-bold"
                         fontSize={20}
@@ -36,6 +48,48 @@ const ProductPageHeader: React.FC = (): JSX.Element => {
                     />
                     Share
                 </SecondaryButton>
+                <Menu
+                    keepMounted
+                    id="simple-menu"
+                    anchorEl={isOpenSharePopup}
+                    onClose={handleCloseSharePopup}
+                    open={Boolean(isOpenSharePopup)}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleCloseSharePopup}>
+                        {/* <FacebookShareButton
+                            url={window.location.href}
+                            style={{ display: 'flex', alignItems: 'center' }}
+                        >
+                            <FacebookIcon size={32} round />
+                            &nbsp;&nbsp;Share on Facebook
+                        </FacebookShareButton> */}
+                        <Link
+                            href={`https://s.meteast.io/aWz73?u=${window.location.href}&`}
+                            style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}
+                        >
+                            <FacebookIcon size={32} round />
+                            &nbsp;&nbsp;Share on Facebook
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseSharePopup}>
+                        {/* <TwitterShareButton
+                            url={window.location.href}
+                            style={{ display: 'flex', alignItems: 'center' }}
+                        >
+                            <TwitterIcon size={32} round />
+                            &nbsp;&nbsp;Share on Twitter
+                        </TwitterShareButton> */}
+                        <Link
+                            href={`https://s.meteast.io/VqYqq?url=${window.location.href}&`}
+                            style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}
+                        >
+                            <TwitterIcon size={32} round />
+                            &nbsp;&nbsp;Share on Twitter
+                        </Link>
+                    </MenuItem>
+                </Menu>
                 <Box
                     position="relative"
                     onMouseEnter={() => setShowReportBtn(true)}
