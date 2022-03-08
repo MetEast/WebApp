3f68049d51af99c1f27e452ff55d2217b1bc86fd
TrueLifeDev <truelife1228@outlook.com>
2022-03-08 02:19:52 -0600

    YourEarnings dlg updated to show Emptybox when no earnings

--- src/components/TransactionDialogs/YourEarnings/index.tsx
+++ src/components/TransactionDialogs/YourEarnings/index.tsx
@@ -5,6 +5,7 @@ import { SecondaryButton } from 'src/components/Buttons/styles';
 import ELAPrice from 'src/components/ELAPrice';
 import ProductBadge from 'src/components/ProductBadge';
 import { TypeYourEarning } from 'src/types/product-types';
+import LooksEmptyBox from 'src/components/profile/LooksEmptyBox';
 
 export interface ComponentProps {
     earnings: Array<TypeYourEarning>;
@@ -18,25 +19,42 @@ const YourEarnings: React.FC<ComponentProps> = ({ onClose, earnings }): JSX.Elem
                 <DialogTitleTypo sx={{ textAlign: 'center' }}>Your Earnings</DialogTitleTypo>
             </Stack>
             <Stack spacing={3} maxHeight={{ xs: 400, md: '100%' }} sx={{ overflowY: 'auto', overflowX: 'hidden' }}>
-                {earnings.map((item, index) => (
-                    <Stack direction="row" justifyContent="space-between" key={`earning-item-${index}`} spacing={3}>
-                        <Stack direction="row" alignItems="center" spacing={1}>
-                            <img src={item.avatar} width={40} height={40} style={{ borderRadius: '100px' }} alt="" />
-                            <Box>
-                                <Typography fontSize={18} fontWeight={700} sx={{ textTransform: 'capitalize' }}>
-                                    {item.title}
-                                </Typography>
-                                <Typography fontSize={14} fontWeight={500}>
-                                    {item.time}
-                                </Typography>
-                            </Box>
-                        </Stack>
-                        <Stack alignItems="flex-end">
-                            <ELAPrice price_ela={item.price} price_ela_fontsize={14} />
-                            <ProductBadge badgeType={item.badge} />
-                        </Stack>
-                    </Stack>
-                ))}
+                {earnings.length === 0 ? (
+                    <LooksEmptyBox />
+                ) : (
+                    <>
+                        {earnings.map((item, index) => (
+                            <Stack
+                                direction="row"
+                                justifyContent="space-between"
+                                key={`earning-item-${index}`}
+                                spacing={3}
+                            >
+                                <Stack direction="row" alignItems="center" spacing={1}>
+                                    <img
+                                        src={item.avatar}
+                                        width={40}
+                                        height={40}
+                                        style={{ borderRadius: '100px' }}
+                                        alt=""
+                                    />
+                                    <Box>
+                                        <Typography fontSize={18} fontWeight={700} sx={{ textTransform: 'capitalize' }}>
+                                            {item.title}
+                                        </Typography>
+                                        <Typography fontSize={14} fontWeight={500}>
+                                            {item.time}
+                                        </Typography>
+                                    </Box>
+                                </Stack>
+                                <Stack alignItems="flex-end">
+                                    <ELAPrice price_ela={item.price} price_ela_fontsize={14} />
+                                    <ProductBadge badgeType={item.badge} />
+                                </Stack>
+                            </Stack>
+                        ))}
+                    </>
+                )}
             </Stack>
             <SecondaryButton fullWidth onClick={onClose}>
                 Close
