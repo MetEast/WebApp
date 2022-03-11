fb10806171961313360c0baf7d57ea8ed9ce3f03
TrueLifeDev <truelife1228@outlook.com>
2022-03-11 01:07:25 -0600

    RemoveNFT dlg updated

--- src/components/Admin/Dialogs/RemoveNFT/index.tsx
+++ src/components/Admin/Dialogs/RemoveNFT/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Stack } from '@mui/material';
+import { Stack, Box, Typography } from '@mui/material';
 import { DialogTitleTypo } from 'src/components/ModalDialog/styles';
 import { PinkButton, SecondaryButton } from 'src/components/Buttons/styles';
-import SearchTextField from '../components/SearchTextField';
 import CustomTextField from 'src/components/TextField';
 
 export interface ComponentProps {
@@ -11,10 +10,25 @@ export interface ComponentProps {
 
 const RemoveNFT: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
     return (
-        <Stack spacing={5} width={520}>
+        <Stack spacing={4} width={520}>
             <Stack alignItems="center">
                 <DialogTitleTypo>Are you Sure?</DialogTitleTypo>
+                <Typography fontSize={16} fontWeight={400}>
+                    You are deleting the following NFT:
+                </Typography>
             </Stack>
+            <Box borderRadius={2} width={180} height={120} overflow="hidden" alignSelf="center">
+                <img
+                    src="/assets/images/blindbox/blindbox-nft-template3.png"
+                    width="100%"
+                    height="100%"
+                    style={{ objectFit: 'cover' }}
+                    alt=""
+                />
+            </Box>
+            <CustomTextField title="NFT TITLE" placeholder="NFT TITLE" />
+            <CustomTextField title="NFT CREATOR" placeholder="NFT CREATOR" />
+            <CustomTextField title="TOKEN ID" placeholder="TOKEN ID" />
             <Stack direction="row" spacing={2}>
                 <SecondaryButton fullWidth onClick={onClose}>
                     close
