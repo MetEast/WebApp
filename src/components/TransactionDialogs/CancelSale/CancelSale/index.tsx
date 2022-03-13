e3443a687f4ce443b5bb6a9e27b6a3896d704d83
blockchaindev91 <tinybear91@outlook.com>
2022-03-14 00:35:43 +0900

    fix: update cancel sale feature

--- src/components/TransactionDialogs/CancelSale/CancelSale/index.tsx
+++ src/components/TransactionDialogs/CancelSale/CancelSale/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Stack, Typography } from '@mui/material';
 import { DialogTitleTypo } from '../../styles';
 import { SecondaryButton, PinkButton } from 'src/components/Buttons/styles';
 import { useSignInContext } from 'src/context/SignInContext';
 import { useDialogContext } from 'src/context/DialogContext';
 import { useSnackbar } from 'notistack';
-import { AbiItem } from 'web3-utils';
-import { METEAST_MARKET_CONTRACT_ABI, METEAST_MARKET_CONTRACT_ADDRESS } from 'src/contracts/METMarket';
 import { essentialsConnector } from 'src/components/ConnectWallet/EssentialsConnectivity';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 import Web3 from 'web3';
 import { isInAppBrowser } from 'src/services/wallet';
 import { useWeb3React } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
+import { callContractMethod } from 'src/components/ContractMethod';
+import { blankContractMethodParam } from 'src/constants/init-constants';
 
 export interface ComponentProps {}
 
@@ -20,6 +20,7 @@ const CancelSale: React.FC<ComponentProps> = (): JSX.Element => {
     const [signInDlgState] = useSignInContext();
     const [dialogState, setDialogState] = useDialogContext();
     const { enqueueSnackbar } = useSnackbar();
+    const [onProgress, setOnProgress] = useState<boolean>(false);
     const walletConnectProvider: WalletConnectProvider = isInAppBrowser()
         ? window.elastos.getWeb3Provider()
         : essentialsConnector.getWalletConnectProvider();
@@ -27,62 +28,6 @@ const CancelSale: React.FC<ComponentProps> = (): JSX.Element => {
     const walletConnectWeb3 = new Web3(
         signInDlgState.loginType === '1' ? (walletConnectProvider as any) : (library?.provider as any),
     );
-
-    const callCancelOrder = async (_orderId: string) => {
-        const accounts = await walletConnectWeb3.eth.getAccounts();
-        const contractAbi = METEAST_MARKET_CONTRACT_ABI;
-        const contractAddress = METEAST_MARKET_CONTRACT_ADDRESS;
-        const marketContract = new walletConnectWeb3.eth.Contract(contractAbi as AbiItem[], contractAddress);
-        const gasPrice = await walletConnectWeb3.eth.getGasPrice();
-        const transactionParams = {
-            from: accounts[0],
-            gasPrice: gasPrice,
-            gas: 5000000,
-            value: 0,
-        };
-        let txHash = '';
-
-        setDialogState({ ...dialogState, waitingConfirmDlgOpened: true });
-        const timer = setTimeout(() => {
-            setDialogState({ ...dialogState, errorMessageDlgOpened: true, waitingConfirmDlgOpened: false });
-        }, 120000);
-        marketContract.methods
-            .cancelOrder(_orderId)
-            .send(transactionParams)
-            .on('transactionHash', (hash: any) => {
-                console.log('transactionHash', hash);
-                txHash = hash;
-                setDialogState({ ...dialogState, waitingConfirmDlgOpened: false });
-                clearTimeout(timer);
-            })
-            .on('receipt', (receipt: any) => {
-                console.log('receipt', receipt);
-                enqueueSnackbar('Cancel sale succeed!', {
-                    variant: 'success',
-                    anchorOrigin: { horizontal: 'right', vertical: 'top' },
-                });
-                setDialogState({
-                    ...dialogState,
-                    cancelSaleDlgOpened: true,
-                    cancelSaleDlgStep: 1,
-                    cancelSaleTxHash: txHash,
-                });
-            })
-            .on('error', (error: any) => {
-                console.error('error', error);
-                enqueueSnackbar('Cancel sale error!', {
-                    variant: 'warning',
-                    anchorOrigin: { horizontal: 'right', vertical: 'top' },
-                });
-                clearTimeout(timer);
-                setDialogState({
-                    ...dialogState,
-                    cancelSaleDlgOpened: false,
-                    errorMessageDlgOpened: true,
-                    waitingConfirmDlgOpened: false,
-                });
-            });
-    };
 
     const handleCancelSale = () => {
         if (dialogState.cancelSaleTxFee > signInDlgState.walletBalance) {
@@ -92,7 +37,47 @@ const CancelSale: React.FC<ComponentProps> = (): JSX.Element => {
             });
             return;
         }
-        callCancelOrder(dialogState.cancelSaleOrderId);
+        setOnProgress(true);
+        setDialogState({ ...dialogState, waitingConfirmDlgOpened: true });
+        const timer = setTimeout(() => {
+            setDialogState({ ...dialogState, errorMessageDlgOpened: true, waitingConfirmDlgOpened: false });
+        }, 120000);
+        callContractMethod(walletConnectWeb3, {
+            ...blankContractMethodParam,
+            contractType: 2,
+            method: 'cancelOrder',
+            price: '0',
+            orderId: dialogState.cancelBidOrderId,
+        })
+            .then((txHash) => {
+                enqueueSnackbar('Cancel sale succeed!', {
+                    variant: 'success',
+                    anchorOrigin: { horizontal: 'right', vertical: 'top' },
+                });
+                setDialogState({
+                    ...dialogState,
+                    cancelSaleDlgOpened: true,
+                    cancelSaleDlgStep: 1,
+                    cancelSaleTxHash: new String(txHash).toString(),
+                    waitingConfirmDlgOpened: false,
+                });
+            })
+            .catch((error) => {
+                enqueueSnackbar(`Cancel sale error: ${error}!`, {
+                    variant: 'warning',
+                    anchorOrigin: { horizontal: 'right', vertical: 'top' },
+                });
+                setDialogState({
+                    ...dialogState,
+                    cancelSaleDlgOpened: false,
+                    waitingConfirmDlgOpened: false,
+                    errorMessageDlgOpened: true,
+                });
+            })
+            .finally(() => {
+                setOnProgress(false);
+                clearTimeout(timer);
+            });
     };
 
     return (
@@ -119,7 +104,7 @@ const CancelSale: React.FC<ComponentProps> = (): JSX.Element => {
                 >
                     Back
                 </SecondaryButton>
-                <PinkButton fullWidth onClick={handleCancelSale}>
+                <PinkButton fullWidth disabled={onProgress} onClick={handleCancelSale}>
                     Cancel sale
                 </PinkButton>
             </Stack>
