365f76ae435227d05fdf65265ca12c0a0ef02168
blockchaindev91 <tinybear91@outlook.com>
2022-03-15 11:47:16 +0900

    fix: remove unused lines

--- src/components/Admin/Dialogs/RemoveNFT/index.tsx
+++ src/components/Admin/Dialogs/RemoveNFT/index.tsx
@@ -4,16 +4,6 @@ import { DialogTitleTypo } from 'src/components/ModalDialog/styles';
 import { PinkButton, SecondaryButton } from 'src/components/Buttons/styles';
 import CustomTextField from 'src/components/TextField';
 import { useSignInContext } from 'src/context/SignInContext';
-import { useDialogContext } from 'src/context/DialogContext';
-import { useSnackbar } from 'notistack';
-import { AbiItem } from 'web3-utils';
-import { METEAST_MARKET_CONTRACT_ABI, METEAST_MARKET_CONTRACT_ADDRESS } from 'src/contracts/METMarket';
-import { essentialsConnector } from 'src/components/ConnectWallet/EssentialsConnectivity';
-import WalletConnectProvider from '@walletconnect/web3-provider';
-import Web3 from 'web3';
-import { isInAppBrowser } from 'src/services/wallet';
-import { useWeb3React } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
 import { AdminNFTItemType } from 'src/types/admin-table-data-types';
 
 export interface ComponentProps {
@@ -23,15 +13,6 @@ export interface ComponentProps {
 
 const RemoveNFT: React.FC<ComponentProps> = ({ token2Remove, onClose }): JSX.Element => {
     const [signInDlgState] = useSignInContext();
-    const [dialogState, setDialogState] = useDialogContext();
-    const { enqueueSnackbar } = useSnackbar();
-    const walletConnectProvider: WalletConnectProvider = isInAppBrowser()
-        ? window.elastos.getWeb3Provider()
-        : essentialsConnector.getWalletConnectProvider();
-    const { library } = useWeb3React<Web3Provider>();
-    const walletConnectWeb3 = new Web3(
-        signInDlgState.loginType === '1' ? (walletConnectProvider as any) : (library?.provider as any),
-    );
 
     return (
         <Stack spacing={4} width={520}>
