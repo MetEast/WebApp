5550311469a5d0b335937d20f89267ff362a8830
TrueLifeDev <truelife1228@outlook.com>
2022-03-17 01:07:37 -0500

    fix: notifications box updated

--- src/components/NotificationsBox/styles.ts
+++ src/components/NotificationsBox/styles.ts
@@ -4,12 +4,14 @@ export const Container = styled(Stack)`
     width: 420px;
     padding: 32px;
     box-sizing: border-box;
-    box-shadow: 0px 4px 40px -26px rgba(0, 20, 39, 0.4);
     border-radius: 32px;
     background: white;
     ${(props) => props.theme.breakpoints.down('sm')} {
         width: 100%;
         height: 100%;
         border-radius: 0;
+    }
+    ${(props) => props.theme.breakpoints.up('md')} {
+        box-shadow: 0px 4px 40px -26px rgba(0, 20, 39, 0.4);
     }
 `;
