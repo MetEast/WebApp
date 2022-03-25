19fc3fb8d1ebe8fb771788288f206074147c6022
blockchaindev91 <tinybear91@outlook.com>
2022-03-25 10:46:32 +0900

    fix: update max purchases number

--- src/components/TransactionDialogs/BuyBlindBox/BuyBlindBox/index.tsx
+++ src/components/TransactionDialogs/BuyBlindBox/BuyBlindBox/index.tsx
@@ -94,7 +94,7 @@ const BuyBlindBox: React.FC<ComponentProps> = (): JSX.Element => {
                     />
                     <IconBtn
                         onClick={() => {
-                            dialogState.buyBlindMaxPurchases > amount && dialogState.buyBlindInstock >= amount && setAmount(amount + 1);
+                            dialogState.buyBlindMaxPurchases > amount && dialogState.buyBlindInstock > amount && setAmount(amount + 1);
                         }}
                     >
                         <Icon icon="ph:plus" color="#1890FF" />
