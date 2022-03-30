c9ceea40dd3101ddd2abe9cad43f598288992275
TrueLifeDev <truelife1228@outlook.com>
2022-03-30 01:02:22 -0500

    Delete product confirm dlg added in MyNFT/created, MyNFT/purchased product pages

--- src/components/ProductPageHeader/index.tsx
+++ src/components/ProductPageHeader/index.tsx
@@ -10,6 +10,8 @@ import { FacebookIcon, TwitterIcon } from 'react-share';
 import { getShorternUrl } from 'src/services/fetch';
 // import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 // import FullscreenIcon from '@mui/icons-material/Fullscreen';
+import ModalDialog from 'src/components/ModalDialog';
+import DeleteProduct from 'src/components/TransactionDialogs/DeleteProduct';
 
 type ReportBtnTooltipProps<P> = P & { show: number };
 
@@ -30,6 +32,8 @@ const ProductPageHeader: React.FC = (): JSX.Element => {
     const [showReportBtn, setShowReportBtn] = useState<boolean>(false);
     const [isOpenSharePopup, setOpenSharePopup] = useState(null);
     const [shortUrl, setShortUrl] = useState<string>('');
+    const [showDeleteDlg, setShowDeleteDlg] = useState<boolean>(false);
+
     const openSharePopupMenu = (event: any) => {
         setOpenSharePopup(event.currentTarget);
     };
@@ -55,121 +59,141 @@ const ProductPageHeader: React.FC = (): JSX.Element => {
         location.pathname.indexOf('/mynft/created') !== -1 || location.pathname.indexOf('/mynft/purchased') !== -1;
 
     return (
-        <Stack direction="row" justifyContent="space-between">
-            <SecondaryButton
-                size="small"
-                sx={{ paddingX: 2.5 }}
-                onClick={() => {
-                    navigate(-1);
-                }}
-            >
-                <Icon
-                    icon="ph:caret-left-bold"
-                    fontSize={20}
-                    color="#1890FF"
-                    style={{ marginLeft: -4, marginRight: 8 }}
-                />
-                Back
-            </SecondaryButton>
-            <Stack direction="row" spacing={1}>
-                <SecondaryButton size="small" sx={{ paddingX: 2.5 }} onClick={openSharePopupMenu}>
+        <>
+            <Stack direction="row" justifyContent="space-between">
+                <SecondaryButton
+                    size="small"
+                    sx={{ paddingX: 2.5 }}
+                    onClick={() => {
+                        navigate(-1);
+                    }}
+                >
                     <Icon
-                        icon="ph:share-network-bold"
+                        icon="ph:caret-left-bold"
                         fontSize={20}
                         color="#1890FF"
                         style={{ marginLeft: -4, marginRight: 8 }}
                     />
-                    Share
+                    Back
                 </SecondaryButton>
-                <Menu
-                    keepMounted
-                    id="simple-menu"
-                    anchorEl={isOpenSharePopup}
-                    onClose={handleCloseSharePopup}
-                    open={Boolean(isOpenSharePopup)}
-                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                    <MenuItem onClick={handleCloseSharePopup}>
-                        {/* <FacebookShareButton
+                <Stack direction="row" spacing={1}>
+                    <SecondaryButton size="small" sx={{ paddingX: 2.5 }} onClick={openSharePopupMenu}>
+                        <Icon
+                            icon="ph:share-network-bold"
+                            fontSize={20}
+                            color="#1890FF"
+                            style={{ marginLeft: -4, marginRight: 8 }}
+                        />
+                        Share
+                    </SecondaryButton>
+                    <Menu
+                        keepMounted
+                        id="simple-menu"
+                        anchorEl={isOpenSharePopup}
+                        onClose={handleCloseSharePopup}
+                        open={Boolean(isOpenSharePopup)}
+                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                    >
+                        <MenuItem onClick={handleCloseSharePopup}>
+                            {/* <FacebookShareButton
                             url={window.location.href}
                             style={{ display: 'flex', alignItems: 'center' }}
                         >
                             <FacebookIcon size={32} round />
                             &nbsp;&nbsp;Share on Facebook
                         </FacebookShareButton> */}
-                        <Link
-                            href={`https://www.facebook.com/sharer/sharer.php?u=${shortUrl}`}
-                            target="_blank"
-                            style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}
-                        >
-                            <FacebookIcon size={32} round />
-                            &nbsp;&nbsp;Share on Facebook
-                        </Link>
-                    </MenuItem>
-                    <MenuItem onClick={handleCloseSharePopup}>
-                        {/* <TwitterShareButton
+                            <Link
+                                href={`https://www.facebook.com/sharer/sharer.php?u=${shortUrl}`}
+                                target="_blank"
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    textDecoration: 'none',
+                                    color: 'black',
+                                }}
+                            >
+                                <FacebookIcon size={32} round />
+                                &nbsp;&nbsp;Share on Facebook
+                            </Link>
+                        </MenuItem>
+                        <MenuItem onClick={handleCloseSharePopup}>
+                            {/* <TwitterShareButton
                             url={window.location.href}
                             style={{ display: 'flex', alignItems: 'center' }}
                         >
                             <TwitterIcon size={32} round />
                             &nbsp;&nbsp;Share on Twitter
                         </TwitterShareButton> */}
-                        <Link
-                            href={`https://twitter.com/share?url=${shortUrl}`}
-                            target="_blank"
-                            style={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'black' }}
-                        >
-                            <TwitterIcon size={32} round />
-                            &nbsp;&nbsp;Share on Twitter
-                        </Link>
-                    </MenuItem>
-                </Menu>
-                <Box
-                    position="relative"
-                    onMouseEnter={() => setShowReportBtn(true)}
-                    onMouseLeave={() => setShowReportBtn(false)}
-                >
-                    <IconBtn onClick={() => setShowReportBtn(!showReportBtn)}>
-                        <Icon icon="ph:dots-three-vertical-bold" color="#1890FF" />
-                    </IconBtn>
-                    <Stack
-                        direction="row"
-                        spacing={1}
-                        paddingTop={1}
-                        display={showReportBtn ? 'flex' : 'none'}
-                        sx={{ position: 'absolute', right: 0, top: '100%' }}
+                            <Link
+                                href={`https://twitter.com/share?url=${shortUrl}`}
+                                target="_blank"
+                                style={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    textDecoration: 'none',
+                                    color: 'black',
+                                }}
+                            >
+                                <TwitterIcon size={32} round />
+                                &nbsp;&nbsp;Share on Twitter
+                            </Link>
+                        </MenuItem>
+                    </Menu>
+                    <Box
+                        position="relative"
+                        onMouseEnter={() => setShowReportBtn(true)}
+                        onMouseLeave={() => setShowReportBtn(false)}
                     >
-                        {!bShowDelBtn && (
-                            <ReportBtnTooltip title="Coming Soon" show={showReportBtn ? 1 : 0}>
-                                <span>
-                                    <PrimaryButton disabled btn_color="pink" size="small" sx={{ paddingX: 2.5 }}>
-                                        <Icon
-                                            icon="ph:megaphone"
-                                            fontSize={20}
-                                            color="rgba(0, 0, 0, 0.26)"
-                                            style={{ marginLeft: -4, marginRight: 8, marginBottom: 2 }}
-                                        />
-                                        Report
-                                    </PrimaryButton>
-                                </span>
-                            </ReportBtnTooltip>
-                        )}
-                        {bShowDelBtn && (
-                            <PrimaryButton size="small" btn_color="pink" sx={{ paddingX: 2.5 }}>
-                                <Icon
-                                    icon="ph:trash"
-                                    fontSize={20}
-                                    color="#EB5757"
-                                    style={{ marginLeft: -4, marginRight: 8, marginBottom: 2 }}
-                                />
-                                {'Delete'}
-                            </PrimaryButton>
-                        )}
-                    </Stack>
-                </Box>
+                        <IconBtn onClick={() => setShowReportBtn(!showReportBtn)}>
+                            <Icon icon="ph:dots-three-vertical-bold" color="#1890FF" />
+                        </IconBtn>
+                        <Stack
+                            direction="row"
+                            spacing={1}
+                            paddingTop={1}
+                            display={showReportBtn ? 'flex' : 'none'}
+                            sx={{ position: 'absolute', right: 0, top: '100%' }}
+                        >
+                            {!bShowDelBtn && (
+                                <ReportBtnTooltip title="Coming Soon" show={showReportBtn ? 1 : 0}>
+                                    <span>
+                                        <PrimaryButton disabled btn_color="pink" size="small" sx={{ paddingX: 2.5 }}>
+                                            <Icon
+                                                icon="ph:megaphone"
+                                                fontSize={20}
+                                                color="rgba(0, 0, 0, 0.26)"
+                                                style={{ marginLeft: -4, marginRight: 8, marginBottom: 2 }}
+                                            />
+                                            Report
+                                        </PrimaryButton>
+                                    </span>
+                                </ReportBtnTooltip>
+                            )}
+                            {bShowDelBtn && (
+                                <PrimaryButton
+                                    size="small"
+                                    btn_color="pink"
+                                    sx={{ paddingX: 2.5 }}
+                                    onClick={() => setShowDeleteDlg(true)}
+                                >
+                                    <Icon
+                                        icon="ph:trash"
+                                        fontSize={20}
+                                        color="#EB5757"
+                                        style={{ marginLeft: -4, marginRight: 8, marginBottom: 2 }}
+                                    />
+                                    {'Delete'}
+                                </PrimaryButton>
+                            )}
+                        </Stack>
+                    </Box>
+                </Stack>
             </Stack>
-        </Stack>
+            <ModalDialog open={showDeleteDlg} onClose={() => setShowDeleteDlg(false)}>
+                <DeleteProduct onClose={() => setShowDeleteDlg(false)} />
+            </ModalDialog>
+        </>
     );
 };
 
