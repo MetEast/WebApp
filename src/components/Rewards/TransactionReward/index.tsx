9bb08daea279098be6c49c01eaa560f9c3f25870
TrueLifeDev <truelife1228@outlook.com>
2022-04-13 02:52:15 -0500

    Transaction reward box updated with component, minor issue fixed

---
+++ src/components/Rewards/TransactionReward/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Stack, Box, Grid, Typography } from '@mui/material';
+import ClaimBox from '../ClaimBox';
+import { Icon } from '@iconify/react';
+
+const TransactionReward: React.FC = (): JSX.Element => {
+    return (
+        <Box borderRadius={3} paddingX={8} paddingY={7} sx={{ background: '#E8F4FF' }}>
+            <Grid container columns={10} columnSpacing={8}>
+                <Grid item xs={6}>
+                    <img src="/assets/images/rewards/transaction-reward.png" width={32} height={32} alt="" />
+                    <Stack direction="row" alignItems="flex-end">
+                        <Typography fontSize={40} fontWeight={500} color="#1890FF">
+                            Transaction reward
+                        </Typography>
+                        <Icon icon="ph:question" fontSize={18} color="#1890FF" style={{ marginBottom: 14 }} />
+                    </Stack>
+                    <Typography fontSize={16} fontWeight={500} color="#1890FF">
+                        After self-trading NFT and the transaction is completed, you can get the number of tokens of
+                        [(number of tokens released on the day/total transaction volume of the day)*corresponding to NFT
+                        transaction volume*05]
+                    </Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <Typography fontSize={20} fontWeight={500} color="#1890FF">
+                        MET{' '}
+                        <Typography fontSize={20} fontWeight={500} color="black" display="inline">
+                            to be claimed
+                        </Typography>
+                    </Typography>
+                    <ClaimBox sx={{ marginTop: 1.5 }} />
+                    <Typography fontSize={14} fontWeight={500} color="#1890FF" marginTop={2.5}>
+                        You can claim rewards every day, or accumulate a one-time claim, your rewards will not
+                        disappear.
+                    </Typography>
+                    <Stack direction="row" justifyContent="space-between" marginTop={2}>
+                        <Typography fontSize={14} fontWeight={500} color="#1890FF">
+                            The most recent receipt:
+                        </Typography>
+                        <Typography fontSize={14} fontWeight={500} color="#1890FF">
+                            --
+                        </Typography>
+                    </Stack>
+                    <Stack direction="row" justifyContent="space-between" marginTop={0.5}>
+                        <Typography fontSize={14} fontWeight={500} color="#1890FF">
+                            Received so far:
+                        </Typography>
+                        <Typography fontSize={14} fontWeight={500} color="#1890FF">
+                            --
+                        </Typography>
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+};
+
+export default TransactionReward;
--- src/pages/RewardsPage/index.tsx
+++ src/pages/RewardsPage/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Box, Stack, Grid, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import Container from 'src/components/Container';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import ClaimBox from 'src/components/Rewards/ClaimBox';
-import { Icon } from '@iconify/react';
+import TransactionReward from 'src/components/Rewards/TransactionReward';
 
 const RewardsPage: React.FC = (): JSX.Element => {
     const theme = useTheme();
@@ -55,59 +54,7 @@ const RewardsPage: React.FC = (): JSX.Element => {
             </Box>
             <Container sx={{ marginTop: 12 }}>
                 <Stack spacing={14}>
-                    <Grid
-                        container
-                        columns={10}
-                        paddingX={8}
-                        paddingY={7}
-                        borderRadius={3}
-                        columnSpacing={8}
-                        sx={{ background: '#E8F4FF' }}
-                    >
-                        <Grid item xs={6}>
-                            <img src="/assets/images/rewards/transaction-reward.png" width={32} height={32} alt="" />
-                            <Stack direction="row" alignItems="flex-end">
-                                <Typography fontSize={40} fontWeight={500} color="#1890FF">
-                                    Transaction reward
-                                </Typography>
-                                <Icon icon="ph:question" fontSize={18} color="#1890FF" style={{ marginBottom: 14 }} />
-                            </Stack>
-                            <Typography fontSize={16} fontWeight={500} color="#1890FF">
-                                After self-trading NFT and the transaction is completed, you can get the number of
-                                tokens of [(number of tokens released on the day/total transaction volume of the
-                                day)*corresponding to NFT transaction volume*05]
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Typography fontSize={20} fontWeight={500} color="#1890FF">
-                                MET{' '}
-                                <Typography fontSize={20} fontWeight={500} color="black" display="inline">
-                                    to be claimed
-                                </Typography>
-                            </Typography>
-                            <ClaimBox sx={{ marginTop: 1.5 }} />
-                            <Typography fontSize={14} fontWeight={500} color="#1890FF" marginTop={2.5}>
-                                You can claim rewards every day, or accumulate a one-time claim, your rewards will not
-                                disappear.
-                            </Typography>
-                            <Stack direction="row" justifyContent="space-between" marginTop={2}>
-                                <Typography fontSize={14} fontWeight={500} color="#1890FF">
-                                    The most recent receipt:
-                                </Typography>
-                                <Typography fontSize={14} fontWeight={500} color="#1890FF">
-                                    --
-                                </Typography>
-                            </Stack>
-                            <Stack direction="row" justifyContent="space-between" marginTop={0.5}>
-                                <Typography fontSize={14} fontWeight={500} color="#1890FF">
-                                    Received so far:
-                                </Typography>
-                                <Typography fontSize={14} fontWeight={500} color="#1890FF">
-                                    --
-                                </Typography>
-                            </Stack>
-                        </Grid>
-                    </Grid>
+                    <TransactionReward />
                 </Stack>
             </Container>
         </Stack>
