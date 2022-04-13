fc91aaff1d6631af42e04493f73f4b6cd14287a0
TrueLifeDev <truelife1228@outlook.com>
2022-04-13 01:23:18 -0500

    Rewards page banner box updated

--- src/pages/RewardsPage/index.tsx
+++ src/pages/RewardsPage/index.tsx
@@ -1,8 +1,58 @@
 import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import Container from 'src/components/Container';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const RewardsPage: React.FC = (): JSX.Element => {
-    return <Stack>RewardsPage</Stack>;
+    const theme = useTheme();
+    const matchDownSm = useMediaQuery(theme.breakpoints.down('sm'));
+
+    return (
+        <Stack>
+            <Box position="relative">
+                <img src="/assets/images/rewards/banner.png" width="100%" height="100%" alt="" />
+                <Box position="absolute" sx={{ inset: 0 }}>
+                    <Container sx={{ height: '100%' }}>
+                        <Stack height="100%" justifyContent="center" alignItems="flex-start">
+                            <Stack direction="row" alignItems="flex-end">
+                                <img
+                                    src="/assets/images/rewards/met-erc20-token-label.png"
+                                    width={matchDownSm ? '70%' : '100%'}
+                                    alt=""
+                                />
+                                <img
+                                    src="/assets/images/rewards/help.png"
+                                    width={matchDownSm ? 12 : 28}
+                                    alt=""
+                                    style={{ marginLeft: matchDownSm ? -4 : -12, marginBottom: matchDownSm ? 12 : 28 }}
+                                />
+                            </Stack>
+                            <Stack marginLeft={1} marginTop={{ xs: 2, md: 0 }}>
+                                <Typography fontSize={{ xs: 16, sm: 36 }} fontWeight={600} color="white">
+                                    The total supply of mining pool
+                                </Typography>
+                                <Typography fontSize={{ xs: 16, sm: 36 }} fontWeight={400} color="white">
+                                    1,000,000,000
+                                </Typography>
+                                <Typography
+                                    fontSize={{ xs: 16, sm: 36 }}
+                                    fontWeight={600}
+                                    color="white"
+                                    marginTop={{ xs: 2, sm: 5 }}
+                                >
+                                    Unlocked 20%
+                                </Typography>
+                                <Typography fontSize={{ xs: 16, sm: 36 }} fontWeight={400} color="white">
+                                    50,000,000
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                    </Container>
+                </Box>
+            </Box>
+        </Stack>
+    );
 };
 
 export default RewardsPage;
