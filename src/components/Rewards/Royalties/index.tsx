ebc0b0fc0635e60b8ac837e2c09a1702d642a6ef
TrueLifeDev <truelife1228@outlook.com>
2022-04-14 03:22:30 -0500

    Royalties rewards box mobile layout updated in Rewards page

--- src/components/Rewards/Royalties/index.tsx
+++ src/components/Rewards/Royalties/index.tsx
@@ -2,26 +2,47 @@ import React from 'react';
 import { Stack, Box, Grid, Typography } from '@mui/material';
 import ClaimBox from '../ClaimBox';
 import { Icon } from '@iconify/react';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 const Royalties: React.FC = (): JSX.Element => {
+    const theme = useTheme();
+    const matchDownMd = useMediaQuery(theme.breakpoints.down('md'));
+
     return (
-        <Box borderRadius={3} paddingX={8} paddingY={7} sx={{ background: '#E8F4FF' }}>
-            <Grid container columns={10} columnSpacing={8}>
-                <Grid item xs={6}>
-                    <img src="/assets/images/rewards/royalties-icon.svg" width={42} height={42} alt="" />
-                    <Stack direction="row" alignItems="flex-end">
-                        <Typography fontSize={40} fontWeight={500} color="#1890FF">
-                            Royalties
-                        </Typography>
-                        <Icon icon="ph:question" fontSize={18} color="#1890FF" style={{ marginBottom: 14 }} />
+        <Box borderRadius={3} paddingX={{ xs: 4, md: 8 }} paddingY={{ xs: 4, md: 7 }} sx={{ background: '#E8F4FF' }}>
+            <Grid container columns={10} columnSpacing={8} rowGap={2.5}>
+                <Grid item xs={10} md={6}>
+                    <Stack
+                        direction={{ xs: 'row', md: 'column' }}
+                        alignItems={{ xs: 'center', md: 'flex-start' }}
+                        spacing={{ xs: 0.5, md: 0 }}
+                    >
+                        <img
+                            src="/assets/images/rewards/royalties-icon.svg"
+                            width={matchDownMd ? 26 : 42}
+                            height={matchDownMd ? 26 : 42}
+                            alt=""
+                        />
+                        <Stack direction="row" alignItems="center" spacing={0.5}>
+                            <Typography fontSize={{ xs: 20, md: 40 }} fontWeight={500} color="#1890FF">
+                                Royalties
+                            </Typography>
+                            <Icon
+                                icon="ph:question"
+                                fontSize={18}
+                                color="#1890FF"
+                                style={{ marginTop: matchDownMd ? 2 : 14 }}
+                            />
+                        </Stack>
                     </Stack>
-                    <Typography fontSize={16} fontWeight={500} color="#1890FF">
+                    <Typography fontSize={16} fontWeight={500} color="#1890FF" marginTop={1}>
                         The minted project can obtain the number of tokens of [(number of tokens released on the
                         day/total transaction volume of the day)*corresponding to NFT transaction volume*0.4] for each
                         transaction.
                     </Typography>
                 </Grid>
-                <Grid item xs={4}>
+                <Grid item xs={10} md={4}>
                     <Typography fontSize={20} fontWeight={500} color="#1890FF">
                         MET{' '}
                         <Typography fontSize={20} fontWeight={500} color="black" display="inline">
