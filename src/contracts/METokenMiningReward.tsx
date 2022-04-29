454fc5b9b7f5ca003e68de0d87d0dac280d30eac
blockchaindev91 <blockchaindev91@outlook.com>
2022-04-30 00:18:41 +0900

    fix: update contract for test env

--- src/contracts/METokenMiningReward.tsx
+++ src/contracts/METokenMiningReward.tsx
@@ -1,4 +1,4 @@
-export const METEAST_MINING_REWARD_TOKEN_CONTRACT_ADDRESS = '0x1C3b0785d786AC82505Be8BeC1A6B6d03634738c';
+export const METEAST_MINING_REWARD_TOKEN_CONTRACT_ADDRESS = '0xe1ce0551FF7a0aE95b3DD904C20235cAd992e70C';
 export const METEAST_MINING_REWARD_TOKEN_CONTRACT_ABI = [
     {
         inputs: [
@@ -240,6 +240,19 @@ export const METEAST_MINING_REWARD_TOKEN_CONTRACT_ABI = [
             },
         ],
         name: 'getReceivedRewardAsStaker',
+        outputs: [
+            {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+            },
+        ],
+        stateMutability: 'view',
+        type: 'function',
+    },
+    {
+        inputs: [],
+        name: 'getStartTime',
         outputs: [
             {
                 internalType: 'uint256',
