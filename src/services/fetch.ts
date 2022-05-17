7bc555c1112fba7832a1b196d7d3b1cd7d16f66d
blockchaindev91 <hisamitsuteru@gmail.com>
2022-05-17 19:32:50 +0900

    fix: update read notification api

--- src/services/fetch.ts
+++ src/services/fetch.ts
@@ -141,14 +141,41 @@ export const getNotificationList = async (address: string) => {
     return _arrNotificationList;
 };
 
-export const markNotificationsAsRead = async (ids: string) => {
-    const resRead = await fetch(
-        `${process.env.REACT_APP_SERVICE_URL}/sticker/api/v1/readNotifications?ids=${ids}`,
-        FETCH_CONFIG_JSON,
-    );
-    const jsonRead = await resRead.json();
-    return jsonRead.code === 200;
-};
+// export const markNotificationsAsRead = async (ids: string) => {
+//     const resRead = await fetch(
+//         `${process.env.REACT_APP_SERVICE_URL}/sticker/api/v1/readNotifications?ids=${ids}`,
+//         FETCH_CONFIG_JSON,
+//     );
+//     const jsonRead = await resRead.json();
+//     return jsonRead.code === 200;
+// };
+
+export const markNotificationsAsRead = (token: string, ids: string) =>
+    new Promise((resolve: (value: boolean) => void, reject: (value: boolean) => void) => {
+        const reqUrl = `${process.env.REACT_APP_SERVICE_URL}/sticker/api/v1/readNotifications`;
+        const reqBody = {
+            token: token,
+            ids: ids,
+        };
+        fetch(reqUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(reqBody),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.code === 200) {
+                    resolve(true);
+                } else {
+                    resolve(false);
+                }
+            })
+            .catch((error) => {
+                reject(false);
+            });
+    });
 
 export const removeNotifications = async (ids: string) => {
     const resRead = await fetch(
