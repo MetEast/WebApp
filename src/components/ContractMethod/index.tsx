432af44896f0b11ad94832d3401755fe147f0758
blockchaindev91 <hisamitsuteru@gmail.com>
2022-05-26 23:52:44 +0900

    fix: change gas limit as 1.5 * estimated gas

--- src/components/ContractMethod/index.tsx
+++ src/components/ContractMethod/index.tsx
@@ -37,7 +37,78 @@ export const callContractMethod = (walletConnectWeb3: Web3, param: TypeContractM
             console.error('error', error);
             reject(error);
         };
-
+        let contractMethod: any = null;
+        switch (param.method) {
+            case 'mint':
+                contractMethod = smartContract.methods.mint(param.tokenId, param.tokenUri, param.royaltyFee);
+                break;
+            case 'burn':
+                contractMethod = smartContract.methods.burn(param.tokenId);
+                break;
+            case 'setApprovalForAll':
+                contractMethod = smartContract.methods.setApprovalForAll(param.operator, param.approved);
+                break;
+            case 'createOrderForSale':
+                contractMethod = smartContract.methods.createOrderForSale(
+                    param.tokenId,
+                    param.quoteToken,
+                    param._price,
+                    param.didUri,
+                    param.isBlindBox,
+                );
+                break;
+            case 'createOrderForSaleBatch':
+                contractMethod = smartContract.methods.createOrderForSaleBatch(
+                    param.tokenIds,
+                    param.quoteTokens,
+                    param._prices,
+                    param.didUri,
+                    param.isBlindBox,
+                );
+                break;
+            case 'createOrderForAuction':
+                contractMethod = smartContract.methods.createOrderForAuction(
+                    param.tokenId,
+                    param.quoteToken,
+                    param._price,
+                    param.endTime,
+                    param.didUri,
+                );
+                break;
+            case 'bidForOrder':
+                contractMethod = smartContract.methods.bidForOrder(param.orderId, param._price, param.didUri);
+                break;
+            case 'changeOrderPrice':
+                contractMethod = smartContract.methods.changeOrderPrice(param.orderId, param._price);
+                break;
+            case 'cancelOrder':
+                contractMethod = smartContract.methods.cancelOrder(param.orderId);
+                break;
+            case 'buyOrder':
+                contractMethod = smartContract.methods.buyOrder(param.orderId, param.didUri);
+                break;
+            case 'buyOrderBatch':
+                contractMethod = smartContract.methods.buyOrderBatch(param.orderIds, param.didUri);
+                break;
+            case 'settleAuctionOrder':
+                contractMethod = smartContract.methods.settleAuctionOrder(param.orderId);
+                break;
+            case 'takeDownOrder':
+                contractMethod = smartContract.methods.takeDownOrder(param.orderId);
+                break;
+            case 'addManager':
+                contractMethod = smartContract.methods.addManager(param.address);
+                break;
+            case 'removeManager':
+                contractMethod = smartContract.methods.removeManager(param.address);
+                break;
+            case 'setBlacklist':
+                contractMethod = smartContract.methods.setBlacklist(param.address, param.approved);
+                break;
+            default:
+                resolve('no action');
+                break;
+        }
         walletConnectWeb3.eth
             .getAccounts()
             .then((_accounts: string[]) => {
@@ -46,10 +117,13 @@ export const callContractMethod = (walletConnectWeb3: Web3, param: TypeContractM
             })
             .then((_gasPrice: string) => {
                 gasPrice = _gasPrice;
+                return contractMethod.estimateGas({ from: accounts[0] });
+            })
+            .then((_estimatedGas: number) => {
                 const transactionParams = {
                     from: accounts[0],
                     gasPrice: gasPrice,
-                    gas: 5000000,
+                    gas: parseInt((_estimatedGas * 1.5).toString()),
                     value: param.price,
                 };
 
@@ -60,8 +134,7 @@ export const callContractMethod = (walletConnectWeb3: Web3, param: TypeContractM
                         .then((success: boolean) => {
                             if (success) resolve('success');
                             else {
-                                smartContract.methods
-                                    .setApprovalForAll(param.operator, param.approved)
+                                contractMethod
                                     .send(transactionParams)
                                     .once('transactionHash', handleTxEvent)
                                     .once('receipt', handleReceiptEvent)
@@ -69,83 +142,6 @@ export const callContractMethod = (walletConnectWeb3: Web3, param: TypeContractM
                             }
                         });
                 } else {
-                    let contractMethod = null;
-                    switch (param.method) {
-                        case 'mint':
-                            contractMethod = smartContract.methods.mint(
-                                param.tokenId,
-                                param.tokenUri,
-                                param.royaltyFee,
-                            );
-                            break;
-                        case 'burn':
-                            contractMethod = smartContract.methods.burn(param.tokenId);
-                            break;
-                        case 'createOrderForSale':
-                            contractMethod = smartContract.methods.createOrderForSale(
-                                param.tokenId,
-                                param.quoteToken,
-                                param._price,
-                                param.didUri,
-                                param.isBlindBox,
-                            );
-                            break;
-                        case 'createOrderForSaleBatch':
-                            contractMethod = smartContract.methods.createOrderForSaleBatch(
-                                param.tokenIds,
-                                param.quoteTokens,
-                                param._prices,
-                                param.didUri,
-                                param.isBlindBox,
-                            );
-                            break;
-                        case 'createOrderForAuction':
-                            contractMethod = smartContract.methods.createOrderForAuction(
-                                param.tokenId,
-                                param.quoteToken,
-                                param._price,
-                                param.endTime,
-                                param.didUri,
-                            );
-                            break;
-                        case 'bidForOrder':
-                            contractMethod = smartContract.methods.bidForOrder(
-                                param.orderId,
-                                param._price,
-                                param.didUri,
-                            );
-                            break;
-                        case 'changeOrderPrice':
-                            contractMethod = smartContract.methods.changeOrderPrice(param.orderId, param._price);
-                            break;
-                        case 'cancelOrder':
-                            contractMethod = smartContract.methods.cancelOrder(param.orderId);
-                            break;
-                        case 'buyOrder':
-                            contractMethod = smartContract.methods.buyOrder(param.orderId, param.didUri);
-                            break;
-                        case 'buyOrderBatch':
-                            contractMethod = smartContract.methods.buyOrderBatch(param.orderIds, param.didUri);
-                            break;
-                        case 'settleAuctionOrder':
-                            contractMethod = smartContract.methods.settleAuctionOrder(param.orderId);
-                            break;
-                        case 'takeDownOrder':
-                            contractMethod = smartContract.methods.takeDownOrder(param.orderId);
-                            break;
-                        case 'addManager':
-                            contractMethod = smartContract.methods.addManager(param.address);
-                            break;
-                        case 'removeManager':
-                            contractMethod = smartContract.methods.removeManager(param.address);
-                            break;
-                        case 'setBlacklist':
-                            contractMethod = smartContract.methods.setBlacklist(param.address, param.approved);
-                            break;
-                        default:
-                            resolve('no action');
-                            break;
-                    }
                     contractMethod
                         .send(transactionParams)
                         .once('transactionHash', handleTxEvent)
