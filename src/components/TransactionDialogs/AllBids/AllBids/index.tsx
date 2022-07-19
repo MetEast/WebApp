46938def17119ab276b4abd6dd80591ee01a0aee
blockchaindev91 <hisamitsuteru@gmail.com>
2022-07-19 21:27:17 +0900

    feat: add skeleton loading to all bids dlg

--- src/components/TransactionDialogs/AllBids/AllBids/index.tsx
+++ src/components/TransactionDialogs/AllBids/AllBids/index.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Stack, Grid, Box, Typography } from '@mui/material';
+import { Stack, Grid, Typography, Skeleton } from '@mui/material';
 import { DialogTitleTypo } from 'src/components/ModalDialog/styles';
-// import { TypeSelectItem } from 'src/types/select-types';
 import { SecondaryButton } from 'src/components/Buttons/styles';
 import ELAPrice from 'src/components/ELAPrice';
+// import { TypeSelectItem } from 'src/types/select-types';
 // import Select from 'src/components/Select';
 // import { SelectTitleBtn } from './styles';
 // import { Icon } from '@iconify/react';
@@ -13,6 +13,7 @@ import { TypeSingleNFTBid } from 'src/types/product-types';
 import { getNFTLatestBids } from 'src/services/fetch';
 import { useParams } from 'react-router-dom';
 import Username from 'src/components/Username';
+import { blankNFTBid } from 'src/constants/init-constants';
 
 export interface ComponentProps {
     onClose: () => void;
@@ -21,6 +22,7 @@ export interface ComponentProps {
 const AllBids: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
     const params = useParams();
     const [signInDlgState] = useSignInContext();
+    const [loadingData, setLoadingData] = useState(true);
     const [bidsList, setBidsList] = useState<Array<TypeSingleNFTBid>>([]);
     const [myBidsList, setMyBidsList] = useState<Array<TypeSingleNFTBid>>([]);
     // const [sortby, setSortby] = useState<TypeSelectItem>();
@@ -35,7 +37,7 @@ const AllBids: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
         const fetchNFTLatestBids = async () => {
             const _NFTBids = await getNFTLatestBids(
                 params.id,
-                signInDlgState.walletAccounts[0],
+                signInDlgState.address,
                 1,
                 1000,
                 // sortby?.value,
@@ -43,13 +45,15 @@ const AllBids: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
             if (!unmounted) {
                 setBidsList(_NFTBids.all);
                 setMyBidsList(_NFTBids.mine);
+                setLoadingData(false);
             }
         };
-        fetchNFTLatestBids().catch(console.error);
+        if ((signInDlgState.isLoggedIn && signInDlgState.address) || !signInDlgState.isLoggedIn)
+            fetchNFTLatestBids().catch(console.error);
         return () => {
             unmounted = true;
         };
-    }, [signInDlgState.walletAccounts, params.id]); // sortby
+    }, [signInDlgState.isLoggedIn, signInDlgState.address, params.id]);
 
     return (
         <Stack spacing={5} width={520}>
@@ -72,7 +76,7 @@ const AllBids: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
                 /> */}
             </Stack>
             <Stack spacing={3}>
-                {signInDlgState.isLoggedIn && myBidsList.length !== 0 && (
+                {!loadingData && signInDlgState.isLoggedIn && myBidsList.length !== 0 && (
                     <>
                         <Grid item xs={12}>
                             <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }}>
@@ -96,49 +100,70 @@ const AllBids: React.FC<ComponentProps> = ({ onClose }): JSX.Element => {
                     </>
                 )}
 
-                {bidsList.length !== 0 && (
-                    <Box>
-                        <Grid container>
+                <Grid container>
+                    <Grid item xs={4}>
+                        <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }}>
+                            User
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }}>
+                            Date
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }} align="right">
+                            Price
+                        </Typography>
+                    </Grid>
+                </Grid>
+                <Grid container marginTop={2.5} rowGap={3} alignItems="center">
+                    {(loadingData ? [...Array(4).fill(blankNFTBid)] : bidsList).map((item, index) => (
+                        <Grid container item key={`all-bids-row-${index}`} columnSpacing={1} rowGap={1}>
                             <Grid item xs={4}>
-                                <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }}>
-                                    User
-                                </Typography>
+                                {loadingData ? (
+                                    <Skeleton
+                                        variant="rectangular"
+                                        animation="wave"
+                                        width="100%"
+                                        height={24}
+                                        sx={{ bgcolor: '#E8F4FF', borderRadius: 2 }}
+                                    />
+                                ) : (
+                                    <Username username={item.user} fontSize={16} fontWeight={700} />
+                                )}
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography fontSize={14} fontWeight={700} sx={{ textTransform: 'uppercase' }}>
-                                    Date
-                                </Typography>
+                                {loadingData ? (
+                                    <Skeleton
+                                        variant="rectangular"
+                                        animation="wave"
+                                        width="100%"
+                                        height={24}
+                                        sx={{ bgcolor: '#E8F4FF', borderRadius: 2 }}
+                                    />
+                                ) : (
+                                    <Typography fontSize={12} fontWeight={500}>
+                                        {item.time}
+                                    </Typography>
+                                )}
                             </Grid>
                             <Grid item xs={4}>
-                                <Typography
-                                    fontSize={14}
-                                    fontWeight={700}
-                                    sx={{ textTransform: 'uppercase' }}
-                                    align="right"
-                                >
-                                    Price
-                                </Typography>
+                                {loadingData ? (
+                                    <Skeleton
+                                        variant="rectangular"
+                                        animation="wave"
+                                        width="100%"
+                                        height={24}
+                                        sx={{ bgcolor: '#E8F4FF', borderRadius: 2 }}
+                                    />
+                                ) : (
+                                    <ELAPrice price_ela={item.price} price_ela_fontsize={14} alignRight={true} />
+                                )}
                             </Grid>
                         </Grid>
-                        <Grid container marginTop={2.5} rowGap={3} alignItems="center">
-                            {bidsList.map((item, index) => (
-                                <Grid container item key={`all-bids-${index}`}>
-                                    <Grid item xs={4}>
-                                        <Username username={item.user} fontSize={16} fontWeight={700} />
-                                    </Grid>
-                                    <Grid item xs={4}>
-                                        <Typography fontSize={12} fontWeight={500}>
-                                            {item.time}
-                                        </Typography>
-                                    </Grid>
-                                    <Grid item xs={4}>
-                                        <ELAPrice price_ela={item.price} price_ela_fontsize={14} alignRight={true} />
-                                    </Grid>
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Box>
-                )}
+                    ))}
+                </Grid>
             </Stack>
             <SecondaryButton fullWidth onClick={onClose}>
                 Close
